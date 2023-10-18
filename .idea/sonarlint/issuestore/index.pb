248689166e58f5d6024098c28f92332c8641d619
Barbara Kulawska <kulawskab@student.agh.edu.pl>
2023-10-18 19:23:23 +0200

    fix commit

--- .idea/sonarlint/issuestore/index.pb
+++
@@ -1,11 +0,0 @@
-
-7
-gradlew,5\b\5bbfa66edb4db3c7c33c5181f43510990d3307f9
-<
-build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
-?
-settings.gradle,0\5\05efc8b1657769a27696d478ded1e95f38737233
-R
-"src/main/java/PredatorPreyApp.java,9\1\91e928ab92f604129607e7e1d06cdc299f9e1c92
-G
-src/main/java/Main.java,e\4\e44a8606ce010ad9b89aafdfcee2ab49afbf6d10
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,26 @@
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import visualization.MainGrid;
 
-public class Main {
+public class Main extends Application {
 
-    public static void main(String[] args) {
-        Application.launch(PredatorPreyApp.class);
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+
+  @Override
+  public void start(Stage primaryStage)
+  {
+    try
+    {
+      primaryStage.setScene(new Scene(new MainGrid(), 800, 800));
+      primaryStage.show();
     }
+    catch(Exception e)
+    {
+      e.printStackTrace();
+    }
+  }
 }
